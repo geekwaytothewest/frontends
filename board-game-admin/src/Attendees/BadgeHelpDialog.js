450529bf1c20db3ebb120410785025c4cc05a415
Mattie Schraeder <mattie@mattie.lgbt>
2026-05-05 20:12:32 -0500

    fix typo, update wording for clarity

--- board-game-admin/src/Attendees/BadgeHelpDialog.js
+++ board-game-admin/src/Attendees/BadgeHelpDialog.js
@@ -7,7 +7,7 @@ import { toggleBadgeHelpDialog } from './attendeesActions';
 const BadgeHelpDialog = ({ saving, isOpen, toggleDialog, selectedAttendee }) => {
   return (
     <SaveDialog
-      headerText='Unable to find an atendee badge?'
+      headerText='Unable to find an attendee badge?'
       buttonText="OK"
       saving={saving}
       disabled={saving}
@@ -17,10 +17,13 @@ const BadgeHelpDialog = ({ saving, isOpen, toggleDialog, selectedAttendee }) => 
       close={toggleDialog}
       onClosed={() => {}}
     >
-    - Ask to see the attendee's Tabletop.Events receipt<br/>
-    - Reference the badge number on the receipt<br/>
+    Step 1: Ask to see the attendee's Tabletop.Events receipt.<br/><br/>
+    Step 2: Validate the receipt and reference the badge number listed.<br/>
     - The badge number on the receipt translates to the last four digits of the Geekway Badge number.<br/>
-    - For example: badge number 123 on the receipt would be 2610123 in the Geekway system.<br/>
+    - For example: badge number 123 on the receipt would be 2610123 in the Geekway system.<br/><br/>
+    Step 3: If the badge is under a different name than on the receipt, it has already been transferred with their approval.<br/><br/>
+    Step 4: If the badge is not found, we have no record of their purchase.<br/><br/>
+    Step 5: Speak with staff to address the issue.
     </SaveDialog>
   );
 };
