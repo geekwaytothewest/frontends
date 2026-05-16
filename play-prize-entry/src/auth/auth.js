79d95b1327e9370e738a4f806c56c330aef990a6
Mattie Schraeder <mattie@mattie.lgbt>
2026-05-16 06:28:05 -0500

    fix login loop

--- play-prize-entry/src/auth/auth.js
+++ play-prize-entry/src/auth/auth.js
@@ -29,7 +29,7 @@ export default class Auth {
   }
 
   handleAuthentication(successCallback) {
-    this.auth0.handleRedirectCallback((err, authResult) => {
+    this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
         history.replace(window.location.pathname);
